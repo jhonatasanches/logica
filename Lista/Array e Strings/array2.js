41447f7f8da16361353e09d873039208a9814cda
Jhonata Sanches <sanches.jhonata@gmail.com>
2020-05-04 19:09:35 -0300

    Adicionado resolução array1

---
+++ Lista/Array e Strings/array2.js
@@ -0,0 +1,17 @@
+var rs = require('readline-sync')
+var jogador1 = rs.question ('Jogador 1, declare seu nome: ')
+var jogador2 = rs.question ('Jogador 2, declare seu nome: ')
+var numeros1 = []
+var numeros2 = []
+
+for (var contador = 0; contador < 5; contador++) {
+    var numerodojogador1 = rs.questionInt ('Jogador 1, escreva 5 numeros de 1 a 10: ')
+    numeros1.push(numerodojogador1);  //push faz ler minha variavel
+}
+for (var contador = 0; contador < 5; contador++) {
+    var numerodojogador2 = rs.questionInt ('Jogador 2, escreva 5 numeros de 1 a 10: ')
+    numeros2.push(numerodojogador2);  //push faz ler minha variavel
+}
+
+console.log(jogador1+ ' você escolheu os numeros: ' +numeros1)
+console.log(jogador2+ ' você escolheu os numeros: ' +numeros2)
---
+++ Lista/Functions/functions1.js
@@ -0,0 +1,9 @@
+var rs = require('readline-sync')
+var numero = rs.questionFloat('Insira um numero:  ')
+
+function volume (circulo){
+    circulo = (4/3)*3,14)*(numero**3)
+    return circulo
+}
+
+console.log(+circulo)
